15bd88def29b61d44c095fb3e511da97fcfee517
Joel Ahlgren (jahn) <joel.ahlgren@tobiidynavox.com>
2020-12-01 16:33:08 +0100

    Fast solution to day 1, and some changes to ProblemCollector/input location.

--- Source/AdventOfCode2020.Tests/Problems/Problem1Tests.cs
+++ Source/AdventOfCode2020.Tests/Problems/Problem1Tests.cs
@@ -6,6 +6,13 @@ namespace AdventOfCode2020.Tests.Problems
     [TestFixture]
     public class Problem1Tests
     {
+        public void TestFindValuesToSum()
+        {
+            var input = new []{ 1721, 979, 366, 299, 675, 1456 };
 
+            var (first, second) = Problem1.FindValuesToSum(input, 2020);
+
+            Assert.AreEqual(2020, first + second);
+        }
     }
 }
--- Source/AdventOfCode2020/Problems/Problem1.cs
+++ Source/AdventOfCode2020/Problems/Problem1.cs
@@ -1,5 +1,11 @@
 namespace AdventOfCode2020.Problems
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using AdventOfCode2020.Utils;
+
     /// <summary>
     /// Solution for <a href="https://adventofcode.com/2020/day/1">Day 1</a>.
     /// </summary>
@@ -11,7 +17,60 @@ namespace AdventOfCode2020.Problems
 
         public override void CalculateSolution()
         {
-            // TODO
+            var start = DateTime.Now;
+            Result.AnswerPartOne = FindAnswerOne(Input.ConvertToInt(), 2020).ToString(CultureInfo.InvariantCulture);
+            Result.TimePartOne = DateTime.Now - start;
+
+            start = DateTime.Now;
+            Result.AnswerPartTwo = FindAnswerTwo(Input.ConvertToInt(), 2020).ToString(CultureInfo.InvariantCulture);
+            Result.TimePartTwo = DateTime.Now - start;
+        }
+
+        internal static int FindAnswerOne(int[] input, int targetSum)
+        {
+            for (var i = 0; i < input.Length; i++)
+            {
+                for (var j = 0; j < input.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    if (input[i] + input[j] == targetSum)
+                    {
+                        return input[i] * input[j];
+                    }
+                }
+            }
+
+            return int.MinValue;
+        }
+
+        internal static int FindAnswerTwo(int[] input, int targetSum)
+        {
+            var start = DateTime.Now;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                for (var j = 0; j < input.Length; j++)
+                {
+                    for (var k = 0; k < input.Length; k++)
+                    {
+                        if (i == j || i == k || j == k)
+                        {
+                            continue;
+                        }
+
+                        if (input[i] + input[j] + input[k] == targetSum)
+                        {
+                            return input[i] * input[j] * input[k];
+                        }
+                    }
+                }
+            }
+
+            return int.MinValue;
         }
     }
 }
