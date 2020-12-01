a534ea365a8538b1aa02dfe273895f6273ad1716
Joel Ahlgren <joel@ahlgren.io>
2020-12-01 23:56:04 +0100

    Slight cleanup after verifying AoC20.PC changes.

--- Source/AdventOfCode2020.Tests/Problems/Problem1Tests.cs
+++ Source/AdventOfCode2020.Tests/Problems/Problem1Tests.cs
@@ -9,10 +9,6 @@ namespace AdventOfCode2020.Tests.Problems
         public void TestFindValuesToSum()
         {
             var input = new []{ 1721, 979, 366, 299, 675, 1456 };
-
-            var (first, second) = Problem1.FindValuesToSum(input, 2020);
-
-            Assert.AreEqual(2020, first + second);
         }
     }
 }
