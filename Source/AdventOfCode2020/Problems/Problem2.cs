7ddd5b3589468c2e15d91b0b552fbc71d5c80043
Joel Ahlgren <joel@ahlgren.io>
2020-12-02 08:05:45 +0000

    Problem Collector: Adding input and boilerplate for 2.

---
+++ Source/AdventOfCode2020/Problems/Problem2.cs
@@ -0,0 +1,17 @@
+namespace AdventOfCode2020.Problems
+{
+    /// <summary>
+    /// Solution for <a href="https://adventofcode.com/2020/day/2">Day 2</a>.
+    /// </summary>
+    public class Problem2 : ProblemBase
+    {
+        public Problem2() : base(2)
+        {
+        }
+
+        public override void CalculateSolution()
+        {
+            // TODO
+        }
+    }
+}
