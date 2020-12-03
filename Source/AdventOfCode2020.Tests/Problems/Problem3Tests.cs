bddc6e773a9f1365e8c5e8fe03d632676f165de4
Joel Ahlgren <joel@ahlgren.io>
2020-12-03 09:23:30 +0100

    Day 3 solved.

--- Source/AdventOfCode2020.Tests/Problems/Problem3Tests.cs
+++ Source/AdventOfCode2020.Tests/Problems/Problem3Tests.cs
@@ -6,6 +6,63 @@ namespace AdventOfCode2020.Tests.Problems
     [TestFixture]
     public class Problem3Tests
     {
+        private static readonly string[] Input = 
+        {
+            "..##.......",
+            "#...#...#.." ,
+            ".#....#..#." ,
+            "..#.#...#.#" ,
+            ".#...##..#." ,
+            "..#.##....." ,
+            ".#.#.#....#" ,
+            ".#........#" ,
+            "#.##...#..." ,
+            "#...##....#" ,
+            ".#..#...#.#"
+        };
 
+        [Test]
+        public void ParseInput_MapSize()
+        {
+            var map = Problem3.ParseInputToMap(Input);
+
+            Assert.AreEqual(11, map.GetLength(0));
+            Assert.AreEqual(11, map.GetLength(1));
+        }
+
+        [Test]
+        public void ParseInput_MapParsing()
+        {
+            var map = Problem3.ParseInputToMap(Input);
+
+            Assert.IsFalse(map[0,0]);
+            Assert.IsTrue(map[0,1]);
+            Assert.IsTrue(map[10, 10]);
+        }
+
+        [TestCase(1, 1, 2)]
+        [TestCase(3, 1, 7)]
+        [TestCase(5, 1, 3)]
+        [TestCase(7, 1, 4)]
+        [TestCase(1, 2, 2)]
+        public void TestSlope(int xSpeed, int ySpeed, int expectedTrees)
+        {
+            Assert.AreEqual(expectedTrees , Problem3.CountHits(Problem3.ParseInputToMap(Input), xSpeed, ySpeed));
+        }
+
+        [Test]
+        public void PartTwoTest()
+        {
+            var map = Problem3.ParseInputToMap(Input);
+
+            var total = Problem3.CountHits(map, 1, 1);
+
+            total *= Problem3.CountHits(map, 3, 1);
+            total *= Problem3.CountHits(map, 5, 1);
+            total *= Problem3.CountHits(map, 7, 1);
+            total *= Problem3.CountHits(map, 1, 2);
+
+            Assert.AreEqual(336, total);
+        }
     }
 }
--- Source/AdventOfCode2020/Problems/Problem3.cs
+++ Source/AdventOfCode2020/Problems/Problem3.cs
@@ -1,5 +1,7 @@
 namespace AdventOfCode2020.Problems
 {
+    using System.Globalization;
+
     /// <summary>
     /// Solution for <a href="https://adventofcode.com/2020/day/3">Day 3</a>.
     /// </summary>
@@ -10,13 +12,57 @@ namespace AdventOfCode2020.Problems
         /// <inheritdoc />
         protected override string SolvePartOne()
         {
-            return "Unsolved";
+            return CountHits(ParseInputToMap(Input), 3, 1).ToString();
         }
 
         /// <inheritdoc />
         protected override string SolvePartTwo()
         {
-            return "Unsolved";
+            var map = ParseInputToMap(Input);
+
+            long total = CountHits(map, 1, 1);
+
+            total *= CountHits(map, 3, 1);
+            total *= CountHits(map, 5, 1);
+            total *= CountHits(map, 7, 1);
+            total *= CountHits(map, 1, 2);
+
+            return total.ToString();
+        }
+
+        internal static bool[,] ParseInputToMap(string[] input)
+        {
+            var result = new bool[input[0].Length, input.Length];
+
+            for (var y = 0; y < input.Length; y++)
+            {
+                for (var x = 0; x < input[y].Length; x++)
+                {
+                    result[x, y] = input[y][x] == '#';
+                }
+            }
+
+            return result;
+        }
+
+        internal static int CountHits(bool[,] map, int xSpeed, int ySpeed)
+        {
+            var (x, y) = (0, 0);
+
+            var trees = 0;
+
+            while (y < map.GetLength(1))
+            {
+                if (map[x%map.GetLength(0), y])
+                {
+                    trees++;
+                }
+
+                x += xSpeed;
+                y += ySpeed;
+            }
+
+            return trees;
         }
     }
 }
