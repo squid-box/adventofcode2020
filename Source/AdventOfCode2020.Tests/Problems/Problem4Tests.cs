7f65ac1adcf81418e0063d4ac912a34aa951abd7
Joel Ahlgren <joel@ahlgren.io>
2020-12-04 09:40:01 +0100

    Day 4 solved, needs cleanup.

--- Source/AdventOfCode2020.Tests/Problems/Problem4Tests.cs
+++ Source/AdventOfCode2020.Tests/Problems/Problem4Tests.cs
@@ -1,11 +1,102 @@
 namespace AdventOfCode2020.Tests.Problems
 {
+    using System.Linq;
     using AdventOfCode2020.Problems;
     using NUnit.Framework;
 
     [TestFixture]
     public class Problem4Tests
     {
+        private string[] FirstInput =
+        {
+            "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
+            "byr:1937 iyr:2017 cid:147 hgt:183cm",
+            "",
+            "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884",
+            "hcl:#cfa07d byr:1929",
+            "",
+            "hcl:#ae17e1 iyr:2013",
+            "eyr:2024",
+            "ecl:brn pid:760753108 byr:1931",
+            "hgt:179cm",
+            "",
+            "hcl:#cfa07d eyr:2025 pid:166559648",
+            "iyr:2011 ecl:brn hgt:59in"
+        };
 
+        private string[] ValidInput =
+        {
+            "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980",
+            "hcl:#623a2f",
+            "",
+            "eyr:2029 ecl:blu cid:129 byr:1989",
+            "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+            "",
+            "hcl:#888785",
+            "hgt:164cm byr:2001 iyr:2015 cid:88",
+            "pid:545766238 ecl:hzl",
+            "eyr:2022",
+            "",
+            "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
+        };
+
+        private string[] InvalidInput =
+        {
+            "eyr:1972 cid:100",
+            "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+            "",
+            "iyr:2019",
+            "hcl:#602927 eyr:1967 hgt:170cm",
+            "ecl:grn pid:012533040 byr:1946",
+            "",
+            "hcl:dab227 iyr:2012",
+            "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+            "",
+            "hgt:59cm ecl:zzz",
+            "eyr:2038 hcl:74454a iyr:2023",
+            "pid:3556412378 byr:2007"
+        };
+
+        [Test]
+        public void Passport_Parsing()
+        {
+            var passport = new Passport(new [] {"hcl:#ae17e1 iyr:2013", "eyr:2024", "ecl:brn pid:760753108 byr:1931", "hgt:179cm",});
+
+            Assert.AreEqual("#ae17e1", passport.HairColor);
+            Assert.AreEqual(2013, passport.IssueYear);
+            Assert.AreEqual(2024, passport.ExpirationYear);
+            Assert.AreEqual("brn", passport.EyeColor);
+            Assert.AreEqual("760753108", passport.PassportId);
+            Assert.IsNull(passport.CountryId);
+            Assert.AreEqual("179cm", passport.Height);
+        }
+
+        [Test]
+        public void Passport_IsValid()
+        {
+            var passports = Problem4.ParseInput(FirstInput).ToList();
+
+            Assert.IsTrue(passports[0].IsValid);
+            Assert.IsFalse(passports[1].IsValid);
+            Assert.IsTrue(passports[2].IsValid);
+            Assert.IsFalse(passports[3].IsValid);
+        }
+
+        [Test]
+        public void Passport_IsStrictlyValid()
+        {
+            var invalidPassports = Problem4.ParseInput(InvalidInput).ToList();
+            var validPassports = Problem4.ParseInput(ValidInput).ToList();
+
+            foreach (var invalidPassport in invalidPassports)
+            {
+                Assert.IsFalse(invalidPassport.IsStrictlyValid);
+            }
+
+            foreach (var validPassport in validPassports)
+            {
+                Assert.IsTrue(validPassport.IsStrictlyValid);
+            }
+        }
     }
 }
--- Source/AdventOfCode2020/Problems/Problem4.cs
+++ Source/AdventOfCode2020/Problems/Problem4.cs
@@ -1,5 +1,10 @@
 namespace AdventOfCode2020.Problems
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using AdventOfCode2020.Utils.Extensions;
+
     /// <summary>
     /// Solution for <a href="https://adventofcode.com/2020/day/4">Day 4</a>.
     /// </summary>
@@ -10,13 +15,155 @@ namespace AdventOfCode2020.Problems
         /// <inheritdoc />
         protected override string SolvePartOne()
         {
-            return "Unsolved";
+            var passports = ParseInput(Input);
+
+            return FindValidPassports(passports).ToString();
         }
 
         /// <inheritdoc />
         protected override string SolvePartTwo()
         {
-            return "Unsolved";
+            var passports = ParseInput(Input);
+
+            return passports.Count(passport => passport.IsStrictlyValid).ToString();
+        }
+
+        internal static int FindValidPassports(IEnumerable<Passport> passports)
+        {
+            return passports.Count(passport => passport.IsValid);
+        }
+
+
+
+        internal static IEnumerable<Passport> ParseInput(string[] input)
+        {
+            var results = new List<Passport>();
+            var passportData = new List<string>();
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var line = input[i];
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    results.Add(new Passport(passportData));
+                    passportData.Clear();
+                    continue;
+                }
+
+                passportData.Add(line);
+            }
+
+            if (passportData.Count != 0)
+            {
+                results.Add(new Passport(passportData));
+            }
+
+            return results;
+        }
+    }
+
+    internal class Passport
+    {
+        public Passport(IEnumerable<string> input)
+        {
+            foreach (var field in string.Join(' ', input).Split(" "))
+            {
+                var fieldSplit = field.Split(':');
+
+                switch (fieldSplit[0])
+                {
+                    case "byr":
+                        BirthYear = fieldSplit[1].ToInt();
+                    break;
+                    case "iyr":
+                        IssueYear = fieldSplit[1].ToInt();
+                        break;
+                    case "eyr":
+                        ExpirationYear = fieldSplit[1].ToInt();
+                        break;
+                    case "hgt":
+                        Height = fieldSplit[1];
+                        break;
+                    case "hcl":
+                        HairColor = fieldSplit[1];
+                        break;
+                    case "ecl":
+                        EyeColor = fieldSplit[1];
+                        break;
+                    case "pid":
+                        PassportId = fieldSplit[1];
+                        break;
+                    case "cid":
+                        CountryId = fieldSplit[1];
+                        break;
+                }
+            }
+        }
+
+        public int? BirthYear { get; }
+
+        public int? IssueYear { get; }
+
+        public int? ExpirationYear { get; }
+
+        public string Height { get; }
+
+        public string HairColor { get; }
+
+        public string EyeColor { get; }
+
+        public string PassportId { get; }
+
+        public string CountryId { get; }
+
+        public bool IsValid =>
+            BirthYear != null &&
+            IssueYear != null &&
+            ExpirationYear != null &&
+            !string.IsNullOrWhiteSpace(Height) &&
+            !string.IsNullOrWhiteSpace(HairColor) &&
+            !string.IsNullOrWhiteSpace(EyeColor) &&
+            PassportId != null;
+
+        public bool IsStrictlyValid =>
+            IsValid &&
+            BirthYear.Value.IsWithin(1920, 2002) &&
+            IssueYear.Value.IsWithin(2010, 2020) &&
+            ExpirationYear.Value.IsWithin(2020, 2030) &&
+            IsHeightValid() &&
+            Regex.IsMatch(HairColor, @"#[a-z0-9]{6}") &&
+            IsEyeColorValid() &&
+            PassportId.Length == 9 && PassportId.All(char.IsDigit);
+
+        private bool IsEyeColorValid()
+        {
+            return !string.IsNullOrWhiteSpace(EyeColor) &&
+                   (EyeColor.Equals("amb") ||
+                   EyeColor.Equals("blu") ||
+                   EyeColor.Equals("brn") ||
+                   EyeColor.Equals("gry") ||
+                   EyeColor.Equals("grn") ||
+                   EyeColor.Equals("hzl") ||
+                   EyeColor.Equals("oth"));
+        }
+
+        private bool IsHeightValid()
+        {
+            if (!string.IsNullOrWhiteSpace(Height))
+            {
+                if (Height.Contains("cm"))
+                {
+                    return Height.Substring(0, Height.Length - 2).ToInt().IsWithin(150, 193);
+                }
+
+                if (Height.Contains("in"))
+                {
+                    return Height.Substring(0, Height.Length - 2).ToInt().IsWithin(59, 76);
+                }
+            }
+
+            return false;
         }
     }
 }
