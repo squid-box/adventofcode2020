d4d3f20e8ae0425d04e411eafc86f39028de1b83
Joel Ahlgren <joel@ahlgren.io>
2020-12-04 05:02:05 +0000

    Problem Collector: Adding input and boilerplate for 4.

---
+++ Source/AdventOfCode2020/Problems/Problem4.cs
@@ -0,0 +1,22 @@
+namespace AdventOfCode2020.Problems
+{
+    /// <summary>
+    /// Solution for <a href="https://adventofcode.com/2020/day/4">Day 4</a>.
+    /// </summary>
+    public class Problem4 : ProblemBase
+    {
+        public Problem4() : base(4) { }
+
+        /// <inheritdoc />
+        protected override string SolvePartOne()
+        {
+            return "Unsolved";
+        }
+
+        /// <inheritdoc />
+        protected override string SolvePartTwo()
+        {
+            return "Unsolved";
+        }
+    }
+}
