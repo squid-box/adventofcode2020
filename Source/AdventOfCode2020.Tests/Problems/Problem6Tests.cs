2e82b2d1dcca75cb14fa21ebb4c90c456df1da85
Joel Ahlgren <joel@ahlgren.io>
2020-12-06 22:08:24 +0100

    Day 6 solved.

--- Source/AdventOfCode2020.Tests/Problems/Problem6Tests.cs
+++ Source/AdventOfCode2020.Tests/Problems/Problem6Tests.cs
@@ -6,6 +6,18 @@ namespace AdventOfCode2020.Tests.Problems
     [TestFixture]
     public class Problem6Tests
     {
+	    private string[] TestInput = { "abc", "", "a", "b", "c", "", "ab", "ac", "", "a", "a", "a", "a", "", "b" };
 
+        [Test]
+	    public void SumOfYesResponses()
+	    {
+            Assert.AreEqual(11, Problem6.FindSumOfYesAnswers(TestInput));
+	    }
+
+	    [Test]
+	    public void SumOfYesResponsesPartTwo()
+	    {
+		    Assert.AreEqual(6, Problem6.FindSumOfYesAnswersPartTwo(TestInput));
+	    }
     }
 }
--- Source/AdventOfCode2020/Problems/Problem6.cs
+++ Source/AdventOfCode2020/Problems/Problem6.cs
@@ -1,6 +1,9 @@
 namespace AdventOfCode2020.Problems
 {
-    /// <summary>
+	using System.Collections.Generic;
+	using System.Linq;
+
+	/// <summary>
     /// Solution for <a href="https://adventofcode.com/2020/day/6">Day 6</a>.
     /// </summary>
     public class Problem6 : ProblemBase
@@ -10,13 +13,86 @@ namespace AdventOfCode2020.Problems
         /// <inheritdoc />
         protected override string SolvePartOne()
         {
-            return "Unsolved";
+            return FindSumOfYesAnswers(Input).ToString();
         }
 
         /// <inheritdoc />
         protected override string SolvePartTwo()
         {
-            return "Unsolved";
+            return FindSumOfYesAnswersPartTwo(Input).ToString();
         }
-    }
+
+        internal static int FindSumOfYesAnswers(IEnumerable<string> input)
+        {
+	        var sum = 0;
+
+	        var groupAnswers = new HashSet<char>();
+
+	        foreach (var line in input)
+	        {
+		        if (string.IsNullOrWhiteSpace(line))
+		        {
+			        sum += groupAnswers.Count;
+                    groupAnswers.Clear();
+
+			        continue;
+		        }
+
+		        foreach (var c in line)
+		        {
+			        groupAnswers.Add(c);
+		        }
+	        }
+
+	        if (groupAnswers.Count > 0)
+	        {
+                sum += groupAnswers.Count;
+	        }
+
+	        return sum;
+        }
+
+        internal static int FindSumOfYesAnswersPartTwo(IEnumerable<string> input)
+        {
+	        var group = new List<string>();
+	        var sum = 0;
+
+			foreach (var line in input)
+			{
+				if (string.IsNullOrWhiteSpace(line))
+				{
+					sum += CountGroupSum(group);
+					group.Clear();
+
+					continue;
+				}
+
+				group.Add(line);
+			}
+
+			if (group.Count != 0)
+			{
+				sum += CountGroupSum(group);
+			}
+
+			return sum;
+        }
+
+        private static int CountGroupSum(ICollection<string> group)
+        {
+	        var groupSize = group.Count;
+	        var results = new Dictionary<char, int>();
+
+			foreach (var person in group)
+			{
+				foreach (var answer in person)
+				{
+					results[answer] = results.ContainsKey(answer) ? results[answer] + 1 : 1;
+				}
+			}
+
+			return results.Count(res => res.Value == groupSize);
+        }
+
+	}
 }
