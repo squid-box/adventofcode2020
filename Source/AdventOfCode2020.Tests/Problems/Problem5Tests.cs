4956447c4ec628df35b8ad4cb177ffce39a855f9
Joel Ahlgren <joel@ahlgren.io>
2020-12-06 22:08:15 +0100

    Day 5 solved, and gently cleaned up. Still not a good solution, just lucky.

--- Source/AdventOfCode2020.Tests/Problems/Problem5Tests.cs
+++ Source/AdventOfCode2020.Tests/Problems/Problem5Tests.cs
@@ -6,6 +6,16 @@ namespace AdventOfCode2020.Tests.Problems
     [TestFixture]
     public class Problem5Tests
     {
-
+        [TestCase("FBFBBFFRLR", 44, 5, 357)]
+        [TestCase("BFFFBBFRRR", 70, 7, 567)]
+        [TestCase("FFFBBBFRRR", 14, 7, 119)]
+        [TestCase("BBFFBBFRLL", 102, 4, 820)]
+	    public void BoardingPass_VerifyParsing(string input, int expectedRow, int expectedColumn, int expectedId)
+	    {
+            var boardingPass = new BoardingPass(input);
+            Assert.AreEqual(expectedRow, boardingPass.Row);
+            Assert.AreEqual(expectedColumn, boardingPass.Column);
+            Assert.AreEqual(expectedId, boardingPass.Id);
+	    }
     }
 }
--- Source/AdventOfCode2020/Problems/Problem5.cs
+++ Source/AdventOfCode2020/Problems/Problem5.cs
@@ -1,6 +1,11 @@
 namespace AdventOfCode2020.Problems
 {
-    /// <summary>
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using AdventOfCode2020.Utils.Extensions;
+
+	/// <summary>
     /// Solution for <a href="https://adventofcode.com/2020/day/5">Day 5</a>.
     /// </summary>
     public class Problem5 : ProblemBase
@@ -10,13 +15,68 @@ namespace AdventOfCode2020.Problems
         /// <inheritdoc />
         protected override string SolvePartOne()
         {
-            return "Unsolved";
+            return ParseInput(Input.WithoutEmptyLines()).Max(p => p.Id).ToString();
         }
 
         /// <inheritdoc />
         protected override string SolvePartTwo()
         {
-            return "Unsolved";
+	        var boardingPasses = 
+		        ParseInput(Input.WithoutEmptyLines())
+		        .OrderBy(p => p.Id)
+		        .ToList();
+			
+	        var ids = new HashSet<int>(Enumerable.Range(boardingPasses.First().Id, boardingPasses.Last().Id));
+			ids.ExceptWith(boardingPasses.Select(p => p.Id));
+
+			// TODO: Actually ensure there's just one (correct) ID in 'ids'.
+            return ids.First().ToString();
+        }
+
+        internal static IEnumerable<BoardingPass> ParseInput(IList<string> input)
+        {
+            return input.Select(line => new BoardingPass(line));
+        }
+    }
+
+    internal class BoardingPass
+    {
+	    public BoardingPass(string input)
+	    {
+		    RawData = input;
+		    Row = BinarySearch(Enumerable.Range(0, 128).ToList(), input.Substring(0, 7).ToList());
+		    Column = BinarySearch(Enumerable.Range(0, 8).ToList(), input.Substring(7, 3).ToList());
+
+		    Id = Row * 8 + Column;
+	    }
+
+        public string RawData { get; }
+
+        public int Row { get; }
+
+        public int Column { get; }
+
+        public int Id { get; }
+
+        private static int BinarySearch(IList<int> list, IList<char> instructions)
+        {
+	        if (instructions.Count == 0)
+	        {
+		        return list[0];
+	        }
+
+	        if (instructions[0].Equals('F') || instructions[0].Equals('L'))
+	        {
+		        list = list.Take(list.Count / 2).ToList();
+	        }
+	        else
+	        {
+		        list = list.Skip(list.Count / 2).ToList();
+	        }
+
+	        instructions.RemoveAt(0);
+
+	        return BinarySearch(list, instructions);
         }
     }
 }
