7518d431f6019d93d9f705c5d9bbadfb98722a29
Joel Ahlgren <joel@ahlgren.io>
2020-12-07 21:53:35 +0100

    Day 7 solved.

--- Source/AdventOfCode2020.Tests/Problems/Problem7Tests.cs
+++ Source/AdventOfCode2020.Tests/Problems/Problem7Tests.cs
@@ -1,11 +1,73 @@
 namespace AdventOfCode2020.Tests.Problems
 {
+    using System.Linq;
     using AdventOfCode2020.Problems;
     using NUnit.Framework;
 
     [TestFixture]
     public class Problem7Tests
     {
+        private readonly string[] _testInputOne =
+        {
+            "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+            "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+            "bright white bags contain 1 shiny gold bag.",
+            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+            "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+            "faded blue bags contain no other bags.",
+            "dotted black bags contain no other bags."
+        };
 
+        private readonly string[] _testInputTwo =
+        {
+            "shiny gold bags contain 2 dark red bags.",
+            "dark red bags contain 2 dark orange bags.",
+            "dark orange bags contain 2 dark yellow bags.",
+            "dark yellow bags contain 2 dark green bags.",
+            "dark green bags contain 2 dark blue bags.",
+            "dark blue bags contain 2 dark violet bags.",
+            "dark violet bags contain no other bags."
+        };
+
+        [Test]
+        public void FindGoldenBagPossibilities()
+        {
+            var bags = Problem7.ParseInput(_testInputOne);
+            Assert.AreEqual(4, Problem7.FindBagsThatCanHoldShinyGoldBag(bags));
+        }
+
+        [Test]
+        public void FindBagsInsideGoldenBag()
+        {
+            var bags = Problem7.ParseInput(_testInputOne);
+            Assert.AreEqual(32, Problem7.FindBagsWithinShinyGoldBag(bags));
+
+            var moreBags = Problem7.ParseInput(_testInputTwo);
+            Assert.AreEqual(126, Problem7.FindBagsWithinShinyGoldBag(moreBags));
+        }
+
+        [Test]
+        public void Parsing_CreatesAllBags()
+        {
+            var bags = Problem7.ParseInput(_testInputOne);
+
+            Assert.AreEqual(9, bags.Count);
+            Assert.IsTrue(bags.Any(bag => bag.Value.Color.Equals("vibrant plum")));
+            Assert.IsFalse(bags.Any(bag => bag.Value.Color.Equals("bright yellow")));
+        }
+
+        [Test]
+        public void Parsing_BagsContainCorrect()
+        {
+            var bags = Problem7.ParseInput(_testInputOne);
+
+            var lightRedBag = bags.First(b => b.Value.Color.Equals("light red")).Value;
+            Assert.AreEqual(1, lightRedBag.Children.First(b => b.Item1.Color.Equals("bright white")).Item1.Children.Count);
+            Assert.AreEqual(2, lightRedBag.Children.First(b => b.Item1.Color.Equals("muted yellow")).Item1.Children.Count);
+
+            Assert.AreEqual(0, bags.First(b => b.Value.Color.Equals("faded blue")).Value.Children.Count);
+        }
     }
 }
--- Source/AdventOfCode2020/Problems/Problem7.cs
+++ Source/AdventOfCode2020/Problems/Problem7.cs
@@ -1,5 +1,10 @@
 namespace AdventOfCode2020.Problems
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using AdventOfCode2020.Utils.Extensions;
+
     /// <summary>
     /// Solution for <a href="https://adventofcode.com/2020/day/7">Day 7</a>.
     /// </summary>
@@ -10,13 +15,93 @@ namespace AdventOfCode2020.Problems
         /// <inheritdoc />
         protected override object SolvePartOne()
         {
-            return "Unsolved";
+            var bags = ParseInput(Input.WithoutEmptyLines());
+
+            return FindBagsThatCanHoldShinyGoldBag(bags);
         }
 
         /// <inheritdoc />
         protected override object SolvePartTwo()
         {
-            return "Unsolved";
+            var bags = ParseInput(Input.WithoutEmptyLines());
+
+            return FindBagsWithinShinyGoldBag(bags);
         }
+
+        internal static int FindBagsThatCanHoldShinyGoldBag(IDictionary<string, Bag> bags)
+        {
+            return bags["shiny gold"].ParentColors().ToHashSet().Count;
+        }
+
+        internal static long FindBagsWithinShinyGoldBag(IDictionary<string, Bag> bags)
+        {
+            return bags["shiny gold"].CountBags();
+        }
+
+        internal static IDictionary<string, Bag> ParseInput(IEnumerable<string> input)
+        {
+            var bags = new Dictionary<string, Bag>();
+
+            foreach (var line in input)
+            {
+                // dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+
+                var firstRegex = 
+                    Regex.Match(line, @"^(?<container>[\w ]+?) bags contain (?<contained>(?<none>no other bags)|(.*))\.$",
+                    RegexOptions.ExplicitCapture);
+                var color = firstRegex.Groups["container"].Value;
+
+                if (!bags.ContainsKey(color))
+                {
+                    bags.Add(color, new Bag(color));
+                }
+
+                // This bag contains no other bags.
+                if (firstRegex.Groups["none"].Success)
+                {
+                    continue;
+                }
+
+                var bag = bags[color];
+                var contains = firstRegex.Groups["contained"].Value.Split(",");
+
+                foreach (var contained in contains)
+                {
+                    var containsRegex = Regex.Match(contained, @"^\s*(?<amount>\d+) (?<color>[\w ]+?) (bag|bags)$", RegexOptions.ExplicitCapture);
+                    var containedColor = containsRegex.Groups["color"].Value;
+
+                    var otherBag = bags.GetValueOrDefault(containedColor) ?? new Bag(containedColor);
+                    if (!bags.ContainsKey(containedColor))
+                    {
+                        bags[containedColor] = otherBag;
+                    }
+
+                    otherBag.Parents.Add(bag);
+                    bag.Children.Add((otherBag, containsRegex.Groups["amount"].Value.ToInt()));
+                }
+            }
+
+            return bags;
+        }
+    }
+
+    internal class Bag
+    {
+        public Bag(string color)
+        {
+            Color = color;
+            Children = new List<(Bag, int)>();
+            Parents = new List<Bag>();
+        }
+
+        public IList<(Bag, int)> Children { get; }
+        
+        public IList<Bag> Parents { get; }
+
+        public string Color { get; }
+
+        public IEnumerable<string> ParentColors() => Parents.SelectMany(p => p.ParentColors().Concat(new[] { p.Color }));
+
+        public long CountBags() => Children.Sum(child => child.Item2) + Children.Sum(child => child.Item1.CountBags() * child.Item2);
     }
 }
