507a0479513344f763f99477e9b75738964dec80
Joel Ahlgren <joel@ahlgren.io>
2020-12-10 05:02:05 +0000

    Problem Collector: Adding input and boilerplate for 10.

---
+++ Source/AdventOfCode2020/Problems/Problem10.cs
@@ -0,0 +1,22 @@
+namespace AdventOfCode2020.Problems
+{
+    /// <summary>
+    /// Solution for <a href="https://adventofcode.com/2020/day/10">Day 10</a>.
+    /// </summary>
+    public class Problem10 : ProblemBase
+    {
+        public Problem10() : base(10) { }
+
+        /// <inheritdoc />
+        protected override object SolvePartOne()
+        {
+            return "Unsolved";
+        }
+
+        /// <inheritdoc />
+        protected override object SolvePartTwo()
+        {
+            return "Unsolved";
+        }
+    }
+}
