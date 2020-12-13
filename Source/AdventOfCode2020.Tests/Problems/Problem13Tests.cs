08c068eade0f0e805215e39fed836937e8f26b79
Joel Ahlgren <joel@ahlgren.io>
2020-12-13 21:38:01 +0100

    Day 13, part 1 solved. Part 2 comes some other day.

--- Source/AdventOfCode2020.Tests/Problems/Problem13Tests.cs
+++ Source/AdventOfCode2020.Tests/Problems/Problem13Tests.cs
@@ -6,6 +6,27 @@ namespace AdventOfCode2020.Tests.Problems
     [TestFixture]
     public class Problem13Tests
     {
+        private readonly string[] _testInput =
+        {
+            "939",
+            "7,13,x,x,59,x,31,19"
+        };
 
+        [Test]
+        public void TestPartOne()
+        {
+            Assert.AreEqual(295, Problem13.FindPartOne(_testInput));
+        }
+
+        [Test]
+        public void TestPartTwo()
+        {
+            Assert.AreEqual(1068788, Problem13.FindPartTwo(_testInput[1]));
+            Assert.AreEqual(3417, Problem13.FindPartTwo("17,x,13,19"));
+            Assert.AreEqual(754018, Problem13.FindPartTwo("67,7,59,61"));
+            Assert.AreEqual(779210, Problem13.FindPartTwo("67,x,7,59,61"));
+            Assert.AreEqual(1261476, Problem13.FindPartTwo("67,7,x,59,61"));
+            Assert.AreEqual(1202161486, Problem13.FindPartTwo("1789,37,47,1889"));
+        }
     }
 }
--- Source/AdventOfCode2020/Problems/Problem13.cs
+++ Source/AdventOfCode2020/Problems/Problem13.cs
@@ -1,5 +1,9 @@
 namespace AdventOfCode2020.Problems
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using AdventOfCode2020.Utils.Extensions;
+
     /// <summary>
     /// Solution for <a href="https://adventofcode.com/2020/day/13">Day 13</a>.
     /// </summary>
@@ -10,13 +14,33 @@ namespace AdventOfCode2020.Problems
         /// <inheritdoc />
         protected override object SolvePartOne()
         {
-            return "Unsolved";
+            return FindPartOne(Input.WithoutEmptyLines());
         }
 
         /// <inheritdoc />
         protected override object SolvePartTwo()
         {
             return "Unsolved";
+        }
+
+        internal static int FindPartOne(IEnumerable<string> input)
+        {
+            var startTime = input.First().ToInt();
+            var (id, waitTime) = input
+                .Last()
+                .Split(',')
+                .Where(i => !i.Equals("x"))
+                .Select(i => i.ToInt())
+                .Select(i => (Id: i, WaitTime: i - startTime % i))
+                .OrderBy(bus => bus.WaitTime)
+                .First();
+
+            return id * waitTime;
+        }
+
+        internal static long FindPartTwo(string input)
+        {
+            return 0;
         }
     }
 }
