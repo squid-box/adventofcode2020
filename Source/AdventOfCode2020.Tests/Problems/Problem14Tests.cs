dfd6c577e8f43fcaa34a7d6c00c3502a20a1c595
Joel Ahlgren <joel@ahlgren.io>
2020-12-14 05:02:05 +0000

    Problem Collector: Adding input and boilerplate for 14.

---
+++ Source/AdventOfCode2020.Tests/Problems/Problem14Tests.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode2020.Tests.Problems
+{
+    using AdventOfCode2020.Problems;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Problem14Tests
+    {
+
+    }
+}
---
+++ Source/AdventOfCode2020/Problems/Problem14.cs
@@ -0,0 +1,22 @@
+namespace AdventOfCode2020.Problems
+{
+    /// <summary>
+    /// Solution for <a href="https://adventofcode.com/2020/day/14">Day 14</a>.
+    /// </summary>
+    public class Problem14 : ProblemBase
+    {
+        public Problem14() : base(14) { }
+
+        /// <inheritdoc />
+        protected override object SolvePartOne()
+        {
+            return "Unsolved";
+        }
+
+        /// <inheritdoc />
+        protected override object SolvePartTwo()
+        {
+            return "Unsolved";
+        }
+    }
+}
