74e09a9d7fd922236a6cc3518493fba97a7e220a
Joel Ahlgren <joel@ahlgren.io>
2020-12-16 12:03:12 +0100

    Day 16, part 1 solved. Part 2 works for test cases but not live input.

--- Source/AdventOfCode2020.Tests/Problems/Problem16Tests.cs
+++ Source/AdventOfCode2020.Tests/Problems/Problem16Tests.cs
@@ -1,11 +1,72 @@
 namespace AdventOfCode2020.Tests.Problems
 {
+    using System.Linq;
     using AdventOfCode2020.Problems;
     using NUnit.Framework;
 
     [TestFixture]
     public class Problem16Tests
     {
+        private readonly string[] _firstTestInput =
+        {
+            "class: 1-3 or 5-7",
+            "row: 6-11 or 33-44",
+            "seat: 13-40 or 45-50",
+            "",
+            "your ticket:",
+            "7,1,14",
+            "",
+            "nearby tickets:",
+            "7,3,47",
+            "40,4,50",
+            "55,2,20",
+            "38,6,12"
+        };
 
+        [Test]
+        public void TestInputParsing()
+        {
+            var (rules, myTicket, otherTickets) = Problem16.ParseInput(_firstTestInput);
+
+            Assert.AreEqual(3, rules.Count());
+            Assert.AreEqual(4, otherTickets.Count());
+
+            Assert.AreEqual(3, myTicket.Fields.Count);
+
+            Assert.AreEqual("class", rules.First().Name);
+        }
+
+        [Test]
+        public void TestScanErrorRate()
+        {
+            var (rules, myTicket, otherTickets) = Problem16.ParseInput(_firstTestInput);
+
+            Assert.AreEqual(71, Problem16.FindErrorScanRate(rules, otherTickets.Append(myTicket)));
+        }
+
+        [Test]
+        public void TestFindDepartureValueProduct()
+        {
+            string[] testInput =
+            {
+                "class: 0-1 or 4-19",
+                "row: 0-5 or 8-19",
+                "seat: 0-13 or 16-19",
+                "",
+                "your ticket:",
+                "11,12,13",
+                "",
+                "nearby tickets:",
+                "3,9,18",
+                "15,1,5",
+                "5,14,9"
+            };
+
+            var (rules, myTicket, otherTickets) = Problem16.ParseInput(testInput);
+
+            Assert.AreEqual(11, Problem16.FindDepartureValueProduct(rules, myTicket, otherTickets, "row"));
+            Assert.AreEqual(12, Problem16.FindDepartureValueProduct(rules, myTicket, otherTickets, "class"));
+            Assert.AreEqual(13, Problem16.FindDepartureValueProduct(rules, myTicket, otherTickets, "seat"));
+        }
     }
 }
--- Source/AdventOfCode2020/Problems/Problem16.cs
+++ Source/AdventOfCode2020/Problems/Problem16.cs
@@ -1,5 +1,10 @@
 namespace AdventOfCode2020.Problems
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using AdventOfCode2020.Utils.Extensions;
+
     /// <summary>
     /// Solution for <a href="https://adventofcode.com/2020/day/16">Day 16</a>.
     /// </summary>
@@ -10,13 +15,182 @@ namespace AdventOfCode2020.Problems
         /// <inheritdoc />
         protected override object SolvePartOne()
         {
-            return "Unsolved";
+            var (rules, myTicket, otherTickets) = ParseInput(Input);
+
+            return FindErrorScanRate(rules, otherTickets.Append(myTicket));
         }
 
         /// <inheritdoc />
         protected override object SolvePartTwo()
         {
-            return "Unsolved";
+            var (rules, myTicket, otherTickets) = ParseInput(Input);
+
+            return FindDepartureValueProduct(rules, myTicket, otherTickets, "departure");
         }
+
+        internal static int FindErrorScanRate(IEnumerable<Rule> rules, IEnumerable<Ticket> tickets)
+        {
+            return tickets.Select(ticket => ticket.ErrorRate(rules)).Sum();
+        }
+
+        internal static int FindDepartureValueProduct(IEnumerable<Rule> rules, Ticket myTicket, IEnumerable<Ticket> tickets, string target)
+        {
+            var validTickets = tickets.Where(ticket => ticket.Valid(rules)).Append(myTicket);
+            var potentialFieldValues = new Dictionary<string, HashSet<int>>();
+
+            foreach (var rule in rules)
+            {
+                var potential = new HashSet<int>();
+
+                for (var i = 0; i < myTicket.Fields.Count; i++)
+                {
+                    if (tickets.All(ticket => rule.Validate(ticket.Fields[i])))
+                    {
+                        potential.Add(i);
+                    }
+                }
+
+                potentialFieldValues.Add(rule.Name, potential);
+            }
+
+            // Name: Index
+            var knownFields = new Dictionary<string, int>();
+
+            while (knownFields.Count < myTicket.Fields.Count)
+            {
+                var knowns = potentialFieldValues.Where(p => p.Value.Count == 1);
+
+                foreach (var known in knowns)
+                {
+                    var knownIndex = known.Value.First();
+                    knownFields.Add(known.Key, knownIndex);
+                    
+                    foreach (var remainingPotential in potentialFieldValues)
+                    {
+                        remainingPotential.Value.Remove(knownIndex);
+                    }
+                }
+            }
+
+            var departureIndexes = knownFields.Where(rule => rule.Key.StartsWith(target));
+
+            var product = 1;
+            
+            foreach (var (_, value) in departureIndexes)
+            {
+                product *= myTicket.Fields[value];
+            }
+
+            return product;
+        }
+
+        internal static (IEnumerable<Rule> Rules, Ticket MyTicket, IEnumerable<Ticket> OtherTickets) ParseInput(IEnumerable<string> input)
+        {
+            var state = "rules";
+
+            var rules = new List<Rule>();
+            Ticket myTicket = null;
+            var otherTickets = new List<Ticket>();
+
+            foreach (var line in input)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+
+                if (line.Equals("your ticket:"))
+                {
+                    state = "ticket";
+                    continue;
+                }
+
+                if (line.Equals("nearby tickets:"))
+                {
+                    state = "tickets";
+                    continue;
+                }
+
+                switch (state)
+                {
+                    case "rules":
+                        rules.Add(new Rule(line));
+                        break;
+                    case "ticket":
+                        myTicket = new Ticket(line);
+                        break;
+                    case "tickets":
+                        otherTickets.Add(new Ticket(line));
+                        break;
+                }
+            }
+
+            return (rules, myTicket, otherTickets);
+        }
+    }
+
+    internal class Rule
+    {
+        private readonly int _firstLowerRange;
+        private readonly int _firstUpperRange;
+        private readonly int _secondLowerRange;
+        private readonly int _secondUpperRange;
+
+        public Rule(string input)
+        {
+            var matches = Regex.Match(input, @"(?<name>.+): (?<firstLowerRange>\d+)-(?<firstUpperRange>\d+) or (?<secondLowerRange>\d+)-(?<secondUpperRange>\d+)", RegexOptions.ExplicitCapture);
+            Name = matches.Groups["name"].Value;
+
+            _firstLowerRange = matches.Groups["firstLowerRange"].Value.ToInt();
+            _firstUpperRange = matches.Groups["firstUpperRange"].Value.ToInt();
+
+            _secondLowerRange = matches.Groups["secondLowerRange"].Value.ToInt();
+            _secondUpperRange = matches.Groups["secondUpperRange"].Value.ToInt();
+        }
+
+        public string Name { get; }
+
+        public bool Validate(int val)
+        {
+            return val.IsWithin(_firstLowerRange, _firstUpperRange) ||
+                   val.IsWithin(_secondLowerRange, _secondUpperRange);
+        }
+    }
+
+    internal class Ticket
+    {
+        public Ticket(string input)
+        {
+            Fields = new List<int>();
+
+            foreach (var field in input.Split(','))
+            {
+                Fields.Add(field.ToInt());
+            }
+        }
+
+        public bool Valid(IEnumerable<Rule> rules) => Fields.All(field => IsFieldValid(field, rules));
+
+        private static bool IsFieldValid(int field, IEnumerable<Rule> rules)
+        {
+            return rules.Any(rule => rule.Validate(field));
+        }
+
+        public int ErrorRate(IEnumerable<Rule> rules)
+        {
+            var sum = 0;
+
+            foreach (var field in Fields)
+            {
+                if (!IsFieldValid(field, rules))
+                {
+                    sum += field;
+                }
+            }
+
+            return sum;
+        }
+
+        public List<int> Fields { get; }
     }
 }
