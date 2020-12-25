f5c3046460c2106f66c3c6cc657b2e4487098807
Joel Ahlgren <joel@ahlgren.io>
2020-12-25 05:02:05 +0000

    Problem Collector: Adding input and boilerplate for 25.

---
+++ Source/AdventOfCode2020/Problems/Problem25.cs
@@ -0,0 +1,22 @@
+namespace AdventOfCode2020.Problems
+{
+    /// <summary>
+    /// Solution for <a href="https://adventofcode.com/2020/day/25">Day 25</a>.
+    /// </summary>
+    public class Problem25 : ProblemBase
+    {
+        public Problem25() : base(25) { }
+
+        /// <inheritdoc />
+        protected override object SolvePartOne()
+        {
+            return "Unsolved";
+        }
+
+        /// <inheritdoc />
+        protected override object SolvePartTwo()
+        {
+            return "Unsolved";
+        }
+    }
+}
